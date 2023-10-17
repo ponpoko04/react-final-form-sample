6b41dc82b1c73b67ce05b3c0ea86d57984ed2854
ponpoko04 <taum0715@gmail.com>
2023-10-18 00:08:39 +0900

    add simple sample of React Fianl Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,157 @@
-import Image from 'next/image'
+'use client'
+import { Form, Field } from 'react-final-form'
+import Styles from './Styles'
 
 export default function Home() {
+  const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+  const onSubmit = async (values: any) => {
+    await sleep(300)
+    window.alert(JSON.stringify(values, (_, value) => value, 2))
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Styles>
+      <Form
+        onSubmit={onSubmit}
+        initialValues={{ stooge: 'larry', employed: false }}
+        render={({ handleSubmit, form, submitting, pristine, values }) => (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>First Name</label>
+              <Field
+                name="firstName"
+                component="input"
+                type="text"
+                placeholder="First Name"
+              />
+            </div>
+            <div>
+              <label>Last Name</label>
+              <Field
+                name="lastName"
+                component="input"
+                type="text"
+                placeholder="Last Name"
+              />
+            </div>
+            <div>
+              <label>Employed</label>
+              <Field name="employed" component="input" type="checkbox" />
+            </div>
+            <div>
+              <label>Favorite Color</label>
+              <Field name="favoriteColor" component="select">
+                <option />
+                <option value="#ff0000">❤️ Red</option>
+                <option value="#00ff00">💚 Green</option>
+                <option value="#0000ff">💙 Blue</option>
+              </Field>
+            </div>
+            <div>
+              <label>Toppings</label>
+              <Field name="toppings" component="select" multiple>
+                <option value="chicken">🐓 Chicken</option>
+                <option value="ham">🐷 Ham</option>
+                <option value="mushrooms">🍄 Mushrooms</option>
+                <option value="cheese">🧀 Cheese</option>
+                <option value="tuna">🐟 Tuna</option>
+                <option value="pineapple">🍍 Pineapple</option>
+              </Field>
+            </div>
+            <div>
+              <label>Sauces</label>
+              <div>
+                <label>
+                  <Field
+                    name="sauces"
+                    component="input"
+                    type="checkbox"
+                    value="ketchup"
+                  />{' '}
+                  Ketchup
+                </label>
+                <label>
+                  <Field
+                    name="sauces"
+                    component="input"
+                    type="checkbox"
+                    value="mustard"
+                  />{' '}
+                  Mustard
+                </label>
+                <label>
+                  <Field
+                    name="sauces"
+                    component="input"
+                    type="checkbox"
+                    value="mayonnaise"
+                  />{' '}
+                  Mayonnaise
+                </label>
+                <label>
+                  <Field
+                    name="sauces"
+                    component="input"
+                    type="checkbox"
+                    value="guacamole"
+                  />{' '}
+                  Guacamole 🥑
+                </label>
+              </div>
+            </div>
+            <div>
+              <label>Best Stooge</label>
+              <div>
+                <label>
+                  <Field
+                    name="stooge"
+                    component="input"
+                    type="radio"
+                    value="larry"
+                  />{' '}
+                  Larry
+                </label>
+                <label>
+                  <Field
+                    name="stooge"
+                    component="input"
+                    type="radio"
+                    value="moe"
+                  />{' '}
+                  Moe
+                </label>
+                <label>
+                  <Field
+                    name="stooge"
+                    component="input"
+                    type="radio"
+                    value="curly"
+                  />{' '}
+                  Curly
+                </label>
+              </div>
+            </div>
+            <div>
+              <label>Notes</label>
+              <Field name="notes" component="textarea" placeholder="Notes" />
+            </div>
+            <div className="buttons">
+              <button type="submit" disabled={submitting || pristine}>
+                Submit
+              </button>
+              <button
+                type="button"
+                onClick={form.reset}
+                disabled={submitting || pristine}
+              >
+                Reset
+              </button>
+            </div>
+            <pre>{JSON.stringify(values, (_, value) => value, 2)}</pre>
+          </form>
+        )}
+      />
+    </Styles>
   )
 }
